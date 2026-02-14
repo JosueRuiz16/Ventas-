19b420df3cf3204d777809d3bcb55210c0228f13
Josue Ruiz <138641334+JosueRuiz16@users.noreply.github.com>
2026-02-13 23:32:15 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-// Service Worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
-
-// Aquí puedes pegar el código de OneSignal cuando lo tengas
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
